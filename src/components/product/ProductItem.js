d1defdff934ffe296a24723c0657ca66ef7f0201
akanksha-d-02 <akankshya2001@gmail.com>
2024-10-16 12:42:37 +0530

    Update ProductItem.js

--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const ProductItem = ({ product }) => {
   return (
     <div className="product-item">
-    <Link to="/home/description">
+    <Link to="/home/description" style={{height: '170px'}}>
       <img src={product.image} alt={product.name}/>
     </Link>
       <span>{product.name}</span>
